35f66b18fcb9b86f858835578d07a833cdf2ca59
williamtansil <williamtansil.public@gmail.com>
2016-10-15 10:33:18 +0700

    first commit

---
+++ smartproperty/smartproperty/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace smartproperty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(0, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 322);
+            this.label1.TabIndex = 0;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(356, 89);
+            this.vScrollBar1.Minimum = 10;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 139);
+            this.vScrollBar1.TabIndex = 1;
+            this.vScrollBar1.Value = 40;
+            this.vScrollBar1.ValueChanged += new System.EventHandler(this.vScrollBar1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ smartproperty/smartproperty/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace smartproperty
+{
+    public partial class Form1 : Form
+    {
+        int lastX, lastY;
+        Bitmap bmp;
+        Graphics g;
+        bool mousedown;
+        int mouseX, mouseY;
+        int posX, posY;
+        public Form1()
+        {
+            mousedown = false;
+            mouseX = 0;
+            mouseY = 0;
+            lastX = lastY = 90;
+            posX = posY = 0;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(label1.Width, label1.Height);
+            g = Graphics.FromImage(bmp);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            ShowGrid(40, posX, posY);
+        }
+
+        private void vScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(label1.Width, label1.Height);
+            g = Graphics.FromImage(bmp);
+            label1.Location = new Point(lastX - vScrollBar1.Value, lastY - vScrollBar1.Value);
+            ShowGrid(vScrollBar1.Value, posX, posY);
+            label1.Refresh();
+        }
+
+        public void ShowGrid(int _range, int _posX, int _posY)
+        {
+            bmp = new Bitmap(label1.Width, label1.Height);
+            g = Graphics.FromImage(bmp);
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    if (i == 1 && j == 1) g.DrawRectangle(new Pen(Brushes.Red, 4f), i * _range + posX, j * _range + posY, _range, _range);
+                    else g.DrawRectangle(new Pen(Brushes.Black, 1f), i * _range + posX, j * _range + posY, _range, _range);
+                }
+            }
+            label1.Image = bmp;
+            g.Dispose();
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) mousedown = true;
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousedown)
+            {
+                mouseX = MousePosition.X - 200;
+                mouseY = MousePosition.Y - 200;
+                posX = mouseX;
+                posY = mouseY;
+                ShowGrid(vScrollBar1.Value, posX, posY);
+            }
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (mousedown) mousedown = false;
+        }
+    }
+}

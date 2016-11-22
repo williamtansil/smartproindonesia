246ec7f976176b9d08159281daa412068fbe86c3
williamtansil <williamtansil.public@gmail.com>
2016-11-22 15:27:01 +0700

    Basic

--- smartproperty/smartproperty/Program.cs
+++ smartproperty/smartproperty/Program.cs
@@ -16,7 +16,7 @@ namespace smartproperty
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-             Application.Run(new Form1());
+             Application.Run(new Designer());
         }
     }
 }
